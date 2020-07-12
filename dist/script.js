c946de320fdf3b57e9942b0e2a47fba022dd9919
Giosue1994 <giosuelentini1994@outlook.com>
2020-07-12 16:07:37 +0200

    burger menu

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
+  
+  // al click mostro la dropdown
   $('.drop-solutions').click(function() {
-    $('.dropdown').toggleClass('visible');
+    $('.dropdown').slideToggle();
   });
+
+  // al click apro e chiudo il menu
+  $('.hamburger').click(function() {
+    $('.hamburger').slideUp();
+    $('.close').slideDown();
+    $('.slide-menu').slideDown();
+  });
+  $('.close').click(function() {
+    $('.close').slideUp();
+    $('.hamburger').slideDown();
+    $('.slide-menu').slideUp();
+  });
+
 });
